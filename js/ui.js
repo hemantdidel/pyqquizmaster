af3d409f62977f62ff4971267ad3f2bfdff1e366
hemantdidel <hemantdidel2002@gmail.com>
2026-07-22 17:16:53 +0530

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,87 @@
+/* ===========================================
+   PYQQuizMaster UI Module
+=========================================== */
 
+const UI = {
+
+    header() {
+
+        return `
+        
+<header class="header">
+
+    <div class="container">
+
+        <div class="logo">
+
+            <a href="index.html">
+
+                <h2>PYQQuizMaster</h2>
+
+            </a>
+
+        </div>
+
+        <nav>
+
+            <ul>
+
+                <li><a href="#">Home</a></li>
+
+                <li><a href="#">Jobs</a></li>
+
+                <li><a href="#">Results</a></li>
+
+                <li><a href="#">Admit Card</a></li>
+
+                <li><a href="#">Notes</a></li>
+
+                <li><a href="#">Quiz</a></li>
+
+            </ul>
+
+        </nav>
+
+    </div>
+
+</header>
+
+        `;
+
+    },
+
+    hero() {
+
+        return `
+
+<section class="hero">
+
+<div class="container">
+
+<h1>India's Smart Exam Preparation Platform</h1>
+
+<p>
+
+Practice PYQs, Mock Tests, Jobs, Results,
+
+Admit Cards, Notes & Current Affairs
+
+all at one place.
+
+</p>
+
+<a href="#" class="btn">
+
+Start Practice
+
+</a>
+
+</div>
+
+</section>
+
+        `;
+
+    }
+
+};
